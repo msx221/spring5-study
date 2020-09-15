c0b7fab136eeb3800a8e498d38323ba215a3a227
feifei <348731402@qq.com>
2020-09-16 00:50:25 +0800

    2020-09-16 第一次提交

---
+++ src/main/java/com/msx/web/day01/StudyDay01.java
@@ -0,0 +1,13 @@
+package com.msx.web.day01;
+
+/**
+ * @Author:msx
+ * @Date:2020/9/16
+ * @Description:com.msx.web.day01
+ * @Version:1.0
+ */
+public class StudyDay01 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
